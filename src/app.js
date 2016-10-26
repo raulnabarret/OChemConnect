1d98538e4d853dcdce48369c6dbeb714e37698f8
raulnabarret <raulnabarret@gmail.com>
2016-10-26 18:49:43 -0500

    Make card draggable

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
-	
-	$(".button-collapse").sideNav()
 
-	$('.carousel').carousel()
-	$('.carousel.carousel-slider').carousel({full_width: true})
+    $('.button-collapse').sideNav()
+
+    $('.carousel').carousel()
+    $('.carousel.carousel-slider').carousel({ full_width: true })
+
+    $(init)
+
+    function init() {
+
+        $('.card-drag').draggable({
+            containment: '#content',
+            stack: '#cardPile div',
+            cursor: 'move',
+            revert: true
+        })
+
+        $('.card-drop').droppable({
+        	accept: '#cardPile div',
+      		hoverClass: 'hovered',
+      		// drop: handleCardDrop
+        })
+    }
 
 })
